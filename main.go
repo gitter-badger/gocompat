d33cd1880c423ae8e459374830dbe884f3ed656a
Stanislav Gatev <mail@s2gatev.com>
2015-12-03 10:06:03 +0200

    Update main program with flags and better messages

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	"encoding/gob"
+	"flag"
 	"fmt"
 	"go/parser"
 	"go/token"
@@ -12,9 +13,16 @@ import (
 	"regexp"
 )
 
+const compatIndexFileName = ".gocompat"
+
 var goFilePattern = regexp.MustCompile(`^.*\.go$`)
 
 var context = &CompatContext{Packages: map[string]*Package{}}
+
+// Flags.
+var (
+	forceStore = flag.Bool("f", false, "Store compatibility index even if the current API is not compatible with the previous version.")
+)
 
 func process(path string, f os.FileInfo, err error) error {
 	if goFilePattern.Match([]byte(path)) {
@@ -29,33 +37,45 @@ func process(path string, f os.FileInfo, err error) error {
 }
 
 func main() {
+	exitMessage := ""
+	exitCode := 0
+	shouldStoreIndex := true
+
+	// Parse command-line options.
+	flag.Parse()
+
 	// Scan project files.
 	filepath.Walk(".", process)
 
-	// If .gocompat is present - compare.
-	if content, err := ioutil.ReadFile("./.gocompat"); err == nil {
+	// If index is present compare current API to the previous version.
+	if content, err := ioutil.ReadFile(compatIndexFileName); err == nil {
 		older := map[string]*Package{}
-		d := gob.NewDecoder(bytes.NewReader([]byte(content)))
-		err = d.Decode(&older)
-		if err != nil {
-			fmt.Println("Error when decoding.", err)
-		}
+		decoder := gob.NewDecoder(bytes.NewReader([]byte(content)))
 
-		if err := ComparePackages(older, context.Packages); err == nil {
-			fmt.Println("OK")
-			os.Exit(0)
+		if err = decoder.Decode(&older); err == nil {
+			if err := ComparePackages(older, context.Packages); err == nil {
+				exitMessage = "OK"
+			} else {
+				exitMessage = "Not OK"
+				exitCode = 1
+				shouldStoreIndex = false
+			}
 		} else {
-			fmt.Println("Not OK")
-			os.Exit(1)
+			fmt.Println("Error when decoding compatibility index.", err)
 		}
 	}
 
-	// Store context objects in .gocompat
-	buffer := bytes.Buffer{}
-	encoder := gob.NewEncoder(&buffer)
-	err := encoder.Encode(context.Packages)
-	if err != nil {
-		fmt.Println("Error when encoding.", err)
+	// Store context objects in index.
+	if shouldStoreIndex || *forceStore {
+		buffer := bytes.Buffer{}
+		encoder := gob.NewEncoder(&buffer)
+		err := encoder.Encode(context.Packages)
+		if err != nil {
+			fmt.Println("Error when encoding compatibility index.", err)
+		}
+		ioutil.WriteFile(compatIndexFileName, buffer.Bytes(), 0644)
 	}
-	ioutil.WriteFile("./.gocompat", buffer.Bytes(), 0644)
+
+	fmt.Println(exitMessage)
+	os.Exit(exitCode)
 }
